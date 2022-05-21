e80d318e3fc28b925c9802175c3d49745a547d1a
Soundwave <PS-Soundwave@users.noreply.github.com>
2022-05-20 19:19:02 -0700

    Robustify clean script

--- clean.ps1
+++ clean.ps1
@@ -1,18 +1,24 @@
+If (Test-Path '.\build32') {
+	Remove-Item -Path '.\build32' -Recurse -Force
+}
+If (Test-Path '.\build64') {
+	Remove-Item -Path '.\build64' -Recurse -Force
+}
 If (Get-ChildItem -Filter 'OBS-Studio-*-Win-x86.zip') {
-	Remove-Item -Path (Get-ChildItem -Filter 'OBS-Studio-*-Win-x86.zip')
+	Remove-Item -Path (Get-ChildItem -Filter 'OBS-Studio-*-Win-x86.zip') -Force
 }
 If (Get-ChildItem -Filter 'OBS-Studio-*-Win-x64.zip') {
-	Remove-Item -Path (Get-ChildItem -Filter 'OBS-Studio-*-Win-x64.zip')
+	Remove-Item -Path (Get-ChildItem -Filter 'OBS-Studio-*-Win-x64.zip') -Force
 }
 If (Test-Path '.\x86') {
-	Remove-Item -Path '.\x86' -Recurse
+	Remove-Item -Path '.\x86' -Recurse -Force
 }
 If (Test-Path '.\x64') {
-	Remove-Item -Path '.\x64' -Recurse
+	Remove-Item -Path '.\x64' -Recurse -Force
 }
 If (Test-Path '.\win-x86-virtualcam.zip') {
-	Remove-Item -Path '.\win-x86-virtualcam.zip'
+	Remove-Item -Path '.\win-x86-virtualcam.zip' -Force
 }
 If (Test-path '.\win-x64-virtualcam.zip') {
-	Remove-Item -Path '.\win-x64-virtualcam.zip'
+	Remove-Item -Path '.\win-x64-virtualcam.zip' -Force
 }
